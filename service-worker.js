bd8dabc9fdf8aaa46cd208ad4f16844d8f444941
fernandoeger <qraeger@gmail.com>
2025-10-24 21:51:18 -0300

    Criar service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'orcamento-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './login.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  // adicione outros arquivos estáticos que desejar cachear
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request).catch(()=>caches.match('./index.html')))
+  );
+});
